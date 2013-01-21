c78cd69c08fa79af84132c26fae7d538cb100737
John-Paul Drawneek <jdrawneek@nationaltheatre.org.uk>
2013-01-21 16:42:53 +0000

    added mongodb

---
+++ roles/mongodb_server.rb
@@ -0,0 +1,16 @@
+name "mongodb_server"
+description "Installs a single mongodb instance"
+all_env = [ 
+	"role[base]",
+	"recipe[mongodb::10gen_repo]",
+	"recipe[mongodb::default]",
+]
+
+run_list(all_env)
+
+env_run_lists(
+	"_default" => all_env, 
+	"prod" => all_env,
+	#"dev" => all_env + ["recipe[php:module_xdebug]"],
+	"dev" => all_env,
+)
